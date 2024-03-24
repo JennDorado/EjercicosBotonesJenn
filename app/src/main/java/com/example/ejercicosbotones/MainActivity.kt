07909325b5a973aa28d6b21d74e55da88fe9be33
jennifer dorado <jenndorado251@gmail.com>
2024-03-23 21:42:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ejercicosbotones/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.ejercicosbotones
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnWelcome = findViewById<Button>(R.id.btnWelcome)
+        btnWelcome.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Hola Gus", Toast.LENGTH_SHORT).show()
+            // Iniciar la segunda actividad
+            val intent = Intent(this@MainActivity, ButtonActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
